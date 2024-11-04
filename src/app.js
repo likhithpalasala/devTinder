9c0d204c329c3cd35f0172ebdd44ecdd3153ac21
likhithpalasala <likhith.palasala24@gmail.com>
2024-11-03 20:20:02 -0600

    Api level sanitization

--- src/app.js
+++ src/app.js
@@ -67,10 +67,30 @@ app.delete("/user", async (req, res) => {
 });
 
 //Update the user data
-app.patch("/user", async (req, res) => {
-     const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+     const userId = req.params?.userId;
     const data = req.body;
+
     try {
+        const allowedUpdates = [
+        
+            "photoUurl",
+             "about", 
+             "gender", 
+             "age", 
+             "skills"
+            ];
+    
+        const isUpdateAllowed = Object.keys(data).every((k) => 
+            allowedUpdates.includes(k)
+        );
+        if(!isUpdateAllowed)  {
+            throw new Error("Update not allowed");
+        }
+        if(data?.skills.length > 10) {
+            throw new Error("Skills cannot be more than 10");
+        }
+
         const userUpadte = await User.findByIdAndUpdate({_id: userId}, data, {
             returnDocument: "after",
             runValidators: true,
