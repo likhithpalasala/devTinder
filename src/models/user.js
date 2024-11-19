855f0e44ba0d3973473958652fc07197b6c88313
likhithpalasala <likhith.palasala24@gmail.com>
2024-11-18 18:51:29 -0600

    APIs, Queries & Indexes in DB

--- src/models/user.js
+++ src/models/user.js
@@ -49,11 +49,15 @@ const userSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        validate(value) {
+        enum: {
+          values: ["male", "female", "others"],
+          message: `{VALUE} is not valid gender type`
+        },
+        /*validate(value) {
             if (!["male", "female", "others" ].includes(value)) {
                 throw new Error("Gender data is not valid!");
             }
-        },
+        }, */
     },
     photoUrl: {
         type: String,
